876f2eaa8397cede633311b42bdb8dcbbf804ca6
Shijing Liu <brendaliu0628@gmail.com>
2020-09-25 19:58:38 -0500

    update sign up and sign in page, forget password, contact us, add css files.

--- js/about.js
+++ js/about.js
@@ -5,7 +5,6 @@ function toggleOpen() {
 }
 
 function toggleActive(e) {
-  debugger;
   console.log(e.propertyName);
   if (e.propertyName.includes("flex")) {
     this.classList.toggle("open-active");
